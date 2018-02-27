59ddc9343e147ba5bd51ca07caa40a65e31b69b1
nilslindkvist <lindkvist.nils@gmail.com>
2018-02-27 16:16:22 +0100

    Login and Register UI

--- Museum/app/src/main/java/project/museum/view/Login.java
+++ Museum/app/src/main/java/project/museum/view/Login.java
@@ -1,17 +1,23 @@
 package project.museum.view;
 
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import project.museum.R;
+import project.museum.RegisterFragment;
 import project.museum.User;
 import project.museum.controller.LoginController;
 
 public class Login extends AppCompatActivity {
     private String username, password;
-    private Language language;
+    private Language language = Language.SV;
     private boolean sound;
     private User newUser;
     private LoginController controller;
+    private RegisterFragment fragment;
 
     private enum Language {
         SV,
@@ -22,15 +28,47 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        fragment = new RegisterFragment();
     }
 
-    public boolean login(String username, String password) {
+    public void loginClick(View view) {
+        EditText text = (EditText)findViewById(R.id.editTextName);
+        username = text.getText().toString();
+        text = (EditText)findViewById(R.id.editTextPassword);
+        password = text.getText().toString();
+    }
+
+    public void registerClick(View view) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        fragmentTransaction.replace(android.R.id.content, fragment, "register")
+                .addToBackStack("register").setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN).commit();
+
+    }
+
+    public boolean loginCheck(String username, String password) {
         return false;
     }
 
-    public String register() {
+    public String registerCheck(User user) {
         return null;
     }
 
+    public boolean toggleSound() {
+        sound = !sound;
+        return sound;
+    }
 
+    public String changeLanguage() {
+        switch(language) {
+            case SV:
+                language = Language.EN;
+                break;
+            case EN:
+                language = Language.SV;
+                break;
+        }
+        return language.toString();
+    }
 }
