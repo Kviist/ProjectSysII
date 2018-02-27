ad283db3060dc74514350dedd20ca512ee8a0c8b
PontusMathiasson <mathiassonpontus@gmail.com>
2018-02-27 14:06:54 +0100

    Första testet hahahahahahahahaah :DDDDDD

--- Museum/app/src/test/java/project/museum/TestFJ001.java
+++ Museum/app/src/test/java/project/museum/TestFJ001.java
@@ -1,13 +1,30 @@
 package project.museum;
 
-import org.junit.Test;
+import org.junit.Before;
+import org.junit.*;
+import project.museum.view.LoginActivity;
+import static org.junit.Assert.assertEquals;
 
 public class TestFJ001 {
 
-    @Test
-    public void FJ001_JU1(){
 
+    private Login login;
 
+    @Before
+    public void setup() {
+        login = new Login();
     }
 
+    @Test
+    public void FJ001_JU1(){
+        String output = login.changeLanguage();
+        assertEquals("Svenska", output);
+    }
+
+    @Test
+    public void FJ001_JU2(){
+        login.changeLanguage();
+        String output = login.changeLanguge();
+        assertEquals("Engelska", output);
+    }
 }
