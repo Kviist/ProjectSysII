b3bad59bf5b499feed75314d2f9585722532eb5b
Jonathan Kvist <jonathan.kvist@gmail.com>
2018-02-27 15:16:02 +0100

    Some test imp

--- Museum/app/src/main/java/project/museum/User.java
+++ Museum/app/src/main/java/project/museum/User.java
@@ -1,5 +1,26 @@
 package project.museum;
 
 public class User {
+    private String userName;
+    private String password;
+    private String email;
+
+    public User(String userName, String password, String email){
+        this.userName = userName;
+        this.password = password;
+        this.email = email;
+    }
+
+    public String getUserName(){
+        return userName;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+    public String getEmail(){
+        return email;
+    }
 
 }
--- Museum/app/src/main/java/project/museum/controller/LoginController.java
+++ Museum/app/src/main/java/project/museum/controller/LoginController.java
@@ -1,4 +1,22 @@
 package project.museum.controller;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import project.museum.User;
+
 public class LoginController {
+
+    public String registerCheck(User user){
+        Pattern p = Pattern.compile("[a-zA-Z0-9]{5,12}");
+        Matcher m = p.matcher(user.getUserName());
+        if(!m.matches()){
+            if(user.getUserName().length() < 5){
+                return "Username is too short";
+            }else if(user.getUserName().length() > 12){
+                return "Username is too long";
+            }
+        }
+        return "Switch views to Start";
+    }
 }
